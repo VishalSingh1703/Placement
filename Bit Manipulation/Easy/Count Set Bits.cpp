8c995c4f788d59b9df9b00fa6ebd2c0d96415828
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-11-22 16:56:09 +0530

    Create Count Set Bits.cpp

---
+++ Bit Manipulation/Easy/Count Set Bits.cpp
@@ -0,0 +1,18 @@
+https://www.codingninjas.com/studio/problems/count-total-set-bits_784?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=DISCUSS
+
+
+int countSetBits(int N)
+{
+    int count = 0;
+    int num = 0;
+    for(int i = 1; i<=N; i++)
+    {
+        num = i;
+        while(num>0)
+        {
+            count += num & 1;
+            num >>= 1;;
+        }
+    }
+    return count;
+}
