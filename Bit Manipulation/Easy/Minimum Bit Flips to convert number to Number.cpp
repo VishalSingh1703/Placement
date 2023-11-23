c93334eb2b138acc3f4f8dcc37591a6f4af042e9
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-11-23 07:35:43 +0530

    Create Minimum Bit Flips to convert number to Number.cpp

---
+++ Bit Manipulation/Easy/Minimum Bit Flips to convert number to Number.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) 
+    {
+        int count = 0;
+        while(max(start, goal)!=0)
+        {
+            if (((start&1)^(goal&1)) == 1) count++;
+            start>>=1;
+            goal>>=1;
+        }
+        return count;
+    }
+};
