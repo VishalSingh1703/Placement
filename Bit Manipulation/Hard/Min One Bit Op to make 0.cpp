5f6c3946cee3dc7faaf5549468848e63631a0efe
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-11-30 09:37:45 +0530

    Create Min One Bit Op to make 0.cpp

---
+++ Bit Manipulation/Hard/Min One Bit Op to make 0.cpp
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/?envType=daily-question&envId=2023-11-30
+
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res;
+        for(res = 0; n>0; n &= n - 1)
+        {
+            res = -(res + (n ^ (n - 1)));
+        }  
+        return abs(res);
+    }
+};
