57f92a0731a41964efcf47941098a60e1bdaa3ea
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-05 13:15:32 +0530

    Create Task Scheduler.cpp

---
+++ Arrays/Hard/Task Scheduler.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/task-scheduler/description/
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> mp;
+        int count = 0;
+        for(char e:tasks)
+        {
+            mp[e]++;
+            count = max(count, mp[e]);
+        }
+        int ans = (count-1)*(n+1);
+        for(auto e:mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+    }
+};
