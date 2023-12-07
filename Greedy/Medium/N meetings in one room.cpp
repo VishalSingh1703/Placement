37d5ce7bfe1be64e182464ac1ec88f76552bc2c1
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-07 12:40:53 +0530

    Create N meetings in one room.cpp

---
+++ Greedy/Medium/N meetings in one room.cpp
@@ -0,0 +1,60 @@
+https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    static bool cmp(pair<int, int> a, pair<int, int> b)
+    {
+        return a.second < b.second;
+    }
+    
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int, int>> nums;
+        for(int i = 0; i<n; i++)
+        {
+            pair<int, int> p = make_pair(start[i], end[i]);
+            nums.push_back(p);
+        }
+        sort(nums.begin(), nums.end(), cmp);
+        int count = 1;
+        int last = nums[0].second;
+        for(int i = 1; i<n; i++)
+        {
+            if(nums[i].first>last)
+            {
+                count++;
+                last = nums[i].second;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
