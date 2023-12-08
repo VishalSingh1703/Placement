c91b97358090ab46b2b11a2580360090ac2c79a1
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-08 10:33:13 +0530

    Create Inorder Traversal.cpp

---
+++ Binary Tree/Easy/Inorder Traversal.cpp
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    static void solve(TreeNode *node, vector<int> &ans)
+    {
+        if(!node) return;
+        solve(node->left, ans);
+        ans.push_back(node->val);
+        solve(node->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
