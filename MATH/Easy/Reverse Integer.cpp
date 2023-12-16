7c5ef2e78eef0ddc40a226856644da35c0bd3c60
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-16 18:41:07 +0530

    Create Reverse Integer.cpp

---
+++ MATH/Easy/Reverse Integer.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/reverse-integer/description/
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        long r = 0;
+        while(x)
+        {
+            r = r*10 + x%10;
+            x /= 10;
+        }
+        if(r>INT_MAX || r<INT_MIN) return 0;
+        return r;
+    }
+};
