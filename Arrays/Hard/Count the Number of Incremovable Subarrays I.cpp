a9197c643176ce7acc54e04c242ae4dca5c4ad8b
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-24 01:02:01 +0530

    Create Count the Number of Incremovable Subarrays I.cpp

---
+++ Arrays/Hard/Count the Number of Incremovable Subarrays I.cpp
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/count-the-number-of-incremovable-subarrays-i/
+
+
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            for(int j = i; j<nums.size(); j++)
+            {
+                int last = -1, flag = 1;
+                for(int k = 0; k < nums.size(); k++)
+                {
+                    if(k >= i && k <= j) continue;
+                    if(nums[k]<=last)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                    last = nums[k];
+                }
+                if(flag) ans++;
+            }
+        }
+        return ans;
+    }
+};
