d846fd264a20f3c6c9141616bebeb00ebb342e5e
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-24 20:22:32 +0530

    Create Move Zeroes.cpp

---
+++ Sliding Window/Medium/Move Zeroes.cpp
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=top-100-liked
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int j = 0;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(nums[i] != 0) swap(nums[i], nums[j++]);
+        }
+    }
+};
