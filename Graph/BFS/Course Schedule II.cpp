bafe2941986b20d3ec426a59a895493dad5bae14
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-29 19:53:56 +0530

    Create Course Schedule II.cpp

---
+++ Graph/BFS/Course Schedule II.cpp
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/course-schedule-ii/
+
+
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& adj) 
+    {
+        vector<int> indegree(n, 0);
+        for(int i = 0; i<n; i++)
+        {
+            for(int it : adj[i]) indegree[it]++;
+        }
+        queue<int> q;
+        for(int i = 0; i<n; i++)
+        {
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(int it : adj[node]) if(--indegree[it] == 0) q.push(it);
+        }
+        return topo;
+    }
+};
