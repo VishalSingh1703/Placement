f7f0b10b5292460aa9a6bb2aa94f934224809151
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2023-12-31 10:13:38 +0530

    Create Find Eventual Safe States.cpp

---
+++ Graph/BFS/Find Eventual Safe States.cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/find-eventual-safe-states/
+
+
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> R(n);
+        vector<int> outdegree(n), safe(n), ans;
+        queue<int> q;
+        for(int i = 0; i<n; i++)
+        {
+            for(int v : graph[i]) R[v].push_back(i);
+            outdegree[i] = graph[i].size();
+            if(!outdegree[i]) q.push(i);
+        }
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            safe[node] = 1;
+            for(int v : R[node])
+            {
+                if(--outdegree[v] == 0) q.push(v);
+            }
+        }
+
+        for(int i = 0; i<n; i++) if(safe[i]) ans.push_back(i);
+        return ans;
+    }
+};
