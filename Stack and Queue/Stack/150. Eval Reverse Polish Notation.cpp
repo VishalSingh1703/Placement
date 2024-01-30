a014c03e7107953c8b7667341841a73c71903f0a
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2024-01-30 18:01:11 +0530

    Create 150. Eval Reverse Polish Notation.cpp

---
+++ Stack and Queue/Stack/150. Eval Reverse Polish Notation.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/evaluate-reverse-polish-notation/description/?envType=daily-question&envId=2024-01-30
+
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) 
+    {
+        unordered_map<string, function<int (int, int) >> map = 
+        {
+            {"+", [] (int a, int b) { return a + b;}},
+            {"-", [] (int a, int b) { return a - b;}},
+            {"*", [] (int a, int b) { return a * b;}},
+            {"/", [] (int a, int b) { return a / b;}},
+        };
+        stack<int> stack;
+        for(string &s : tokens)
+        {
+            if(!map.count(s)) stack.push(stoi(s));
+            else 
+            {
+                int op1 = stack.top();
+                stack.pop();
+                int op2 = stack.top();
+                stack.pop();
+                stack.push(map[s](op2, op1));
+            }
+        }
+        return stack.top();
+    }
+};
