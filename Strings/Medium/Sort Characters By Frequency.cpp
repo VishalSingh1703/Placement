0ff309c3378e4d2af00d7b99309a5af33c4b60f3
Vishal Singh <113925017+VishalSingh1703@users.noreply.github.com>
2024-02-07 15:10:38 +0530

    Create Sort Characters By Frequency.cpp

---
+++ Strings/Medium/Sort Characters By Frequency.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/sort-characters-by-frequency/?envType=daily-question&envId=2024-02-07
+
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        vector<string> bucket(s.size()+1, "");
+        string res;
+
+        for(char c:s) freq[c]++;
+        for(auto it:freq) bucket[it.second].append(it.second, it.first);
+        for(int i = s.size(); i>0; i--) if(!bucket[i].empty()) res.append(bucket[i]);
+        return res;
+    }
+};
